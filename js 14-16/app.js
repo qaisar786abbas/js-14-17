f124229504e0aaaa824c05e79ef75c4d9e04765b
Qaisar Abbas <122449496+qaisar786abbas@users.noreply.github.com>
2023-02-13 14:36:56 +0500

    added files

---
+++ js 14-16/app.js
@@ -0,0 +1,197 @@
+// >>>>>>>>>>CHAPTER NO 14-16<<<<<<<<<<<<<
+
+
+//Question no 01
+
+// var students = [];
+//console.log(students);
+
+//Question no 02
+
+// var students = new array();
+// console.log(students); 
+
+//Question no 03
+// var students = ["qaisar", "abbas", "asim"]
+// console.log(students);
+// alert(students);
+
+//Question no 04
+
+// var numbers = [3,5,7,8];
+// alert(numbers);
+
+//Question no 05
+// var boolean = ["true", "false"]
+// alert(boolean);
+
+//Question no 06
+
+// var mixed = ["qaisar", 5 , "true"]
+// alert(mixed);
+
+//Question no 07
+
+// var qualifications = ["SSC, HSC, BSC, BS, BCOM, MS, M-PHIL, PHD"]
+// console.log(qualifications);
+// document.write("<h2>Qualifications:<br>1) SSC<br>2) HSC<br>3) BSC<br>4) BS<br>5) BCOM<br>6) MS<br>7) M-PHIL<br>8) PHD</h1>");
+ 
+//Question no 08
+
+// var studentNames = ["qaisar", "mudassir", "johar"]
+// console.log(studentNames);
+// var marks = ["320", "230", "480"]
+// console.log(marks);
+// var total = 500;
+// console.log(total);
+// // var studen = 320;
+// var total1 = [320/500*100 + "%", 230/500*100 + "%", 480/500*100 + "%"]
+// console.log(total1); 
+// document.write("<h1>Score of qaisar is 320.Percentage is: 64%.<h1>");
+// document.write("<h1>Score of mudassir is 230.Percentage is: 46%.<h1>");
+// document.write("<h1>Score of johar is 480.Percentage is: 96%.<h1>");
+
+//Question no 09
+
+// var color = ["red", "blue", "green", "orange", "pink"]
+// console.log(color);
+// document.write("red<br> blue<br> green<br> orange<br> pink");
+
+// //(a)..... first add.
+// color.unshift("brown");
+// console.log(color);
+// document.write(color);
+
+// //(b)......end add
+// color.push("purple");
+// console.log(color);
+// document.write(color);
+
+// //(c)
+// color.unshift("gray","voilet");
+// console.log(color);
+// document.write(color);
+
+// //(d)
+// color.splice(4 , 1);
+// console.log(color);
+// document.write(color);
+
+// //(e)
+// color.shift("gray","voilet");
+// console.log(color);
+// document.write(color);
+
+// //(f)
+// color.splice(4 , 0, "gray");
+// console.log(color);
+// document.write(color);
+
+// //(g)
+// color.splice(5 , 1);
+// console.log(color);
+// document.write(color);
+
+
+//Question no 10
+
+// var scores = [220 , 230 , 480 , 120]
+// console.log(scores);
+// scores.unshift(120);
+// scores.splice(4 , 1);
+// console.log(scores);
+// document.write("<h1>Scores of students : 220 , 230 , 480 , 120<br>Ordered scores of student : 120 , 220 , 230 , 480</h1>");
+
+//Question no 11
+
+// var cities = ["karachi", "lahore", "islamabad", "quetta", "peshawer"]
+// console.log(cities);
+// var selectedCities = cities.slice(2 , 4);
+// console.log(selectedCities);
+// document.write("<h1>Cities list :<br>Karachi , Lahore , Islamabad , Quetta , Peshawer</h1>")
+// document.write("<h1>Selected Cities list :<br> Islamabad , Quetta")
+
+//Question no 12
+
+// var arr = ["This" , " is ", " my ", " cat"];
+// console.log(arr);
+// var arr1 = ["This is my cat"]
+// console.log(arr1);
+// document.write("<h1>Array :<br> This , is , my , cat.</h1>");
+// document.write("<h1>String :<br> This is my cat.</h1>");
+
+//Question no 13
+
+// var hardware = []
+// console.log(hardware);
+// hardware.push("keyboard");
+// console.log(hardware);
+// hardware.push("mouse");
+// console.log(hardware);
+// hardware.push("printer");
+// console.log(hardware);
+// hardware.push("monitor");
+// console.log(hardware);
+// hardware.shift("keyboard");
+// console.log(hardware);
+// hardware.shift("mouse");
+// console.log(hardware);
+// hardware.shift("printer");
+// console.log(hardware);
+// hardware.shift("monitor");
+// console.log(hardware);
+// document.write("<h1>Devices : <br> Keyboard , mouse , printer , monitor.</h1>");
+// document.write("<h1>Out : <br> keyboard. <br> Out : <br> Mouse. <br> Out : <br> Printer. <br> Out : <br> Monitor.</h1>");
+
+//Question no 14
+
+// var hardware = []
+// console.log(hardware);
+// hardware.push("keyboard");
+// console.log(hardware);
+// hardware.push("mouse");
+// console.log(hardware);
+// hardware.push("printer");
+// console.log(hardware);
+// hardware.push("monitor");
+// console.log(hardware);
+// hardware.pop("moniter");
+// console.log(hardware);
+// hardware.pop("printer");
+// console.log(hardware);
+// hardware.pop("mouse");
+// console.log(hardware);
+// hardware.pop("keyboard");
+// console.log(hardware);
+// document.write("<h1>Devices : <br> Keyboard , mouse , printer , monitor.</h1>");
+// document.write("<h1>Out : <br> Monitor. <br> Out : <br> Printer. <br> Out : <br> Mouse. <br> Out : <br> Keyboard.</h1>");
+
+//Question no 15
+
+// var manufacturers = ["Apple", "Samsung", "Motorola", "Nokia", "Sony & Haier"]
+// console.log(manufacturers);
+// document.write("<h1>Manufacturers : <br> 1- Apple <br> 2- Samsung <br> 3- Motorola <br> 4- Nokia <br> 5- Sony & Haier</h1>");
+// // documents.write(menufacturers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
